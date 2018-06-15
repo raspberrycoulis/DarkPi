5aa4be23a78d92d1e0a47785bbc5bbfe22c4ab1d
Wesley Archer <wesleyarcher@hotmail.com>
2018-06-15 14:55:26 +0100

    Added PaPiRus example

--- darksky-papirus.py
+++ darksky-papirus.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from dot3k import joystick as nav
-from dot3k import lcd
-from dot3k import backlight as backlight
-backlight.use_rbg() # Required for early-batch DOT3K's as the RGB LEDs are RBG.
+from papirus import Papirus, PapirusText, PapirusTextPos
 from time import sleep
 import os
 
@@ -32,64 +29,19 @@ humidity = str(humidity)
 rainInt = current.precipProbability*100
 rain = str(rainInt)
 
-def updateWeather():
-    forecast = forecastio.load_forecast(api_key,latitude,longitude)
-
-def rainWarning():
-    if rainInt <= 33:
-        backlight.rgb(165, 255, 0)    # Green (good)
-    elif (rainInt > 34) and (rainInt <= 74):
-        backlight.rgb(226, 111, 11) # Orange (warning)
-    else:
-        backlight.rgb(0, 225, 255)  # Blue (you'll get wet!)
-
-def graph():
-    if rainInt ==0:
-        backlight.set_graph(0)
-    elif (rainInt >1) and (rainInt <=9):
-        backlight.set_graph(0.1)
-    elif (rainInt >= 10) and (rainInt <=19):
-        backlight.set_graph(0.2)
-    elif (rainInt >= 20) and (rainInt <=29):
-        backlight.set_graph(0.3)
-    elif (rainInt >= 30) and (rainInt <=39):
-        backlight.set_graph(0.4)
-    elif (rainInt >= 40) and (rainInt <=49):
-        backlight.set_graph(0.5)
-    elif (rainInt >= 50) and (rainInt <=59):
-        backlight.set_graph(0.6)
-    elif (rainInt >= 60) and (rainInt <=69):
-        backlight.set_graph(0.7)
-    elif (rainInt >= 70) and (rainInt <=79):
-        backlight.set_graph(0.8)
-    elif (rainInt >= 80) and (rainInt <=89):
-        backlight.set_graph(0.9)
-    else:
-        backlight.set_graph(1.0)
-
-# Press the button on the joystick to exit
-@nav.on(nav.BUTTON)
-def handle_button(pin):
-    lcd.clear()
-    backlight.rgb(0, 0, 0)
-    backlight.set_graph(0)
-    os._exit(1)
-
 try:
-    lcd.clear()
-    lcd.set_cursor_position(0, 0)
+    papirus.clear()
+#    lcd.set_cursor_position(0, 0)
     print("Temperture: "+temp+" °C")
-    lcd.write("Temp: "+temp+" C")
-    lcd.set_cursor_position(0, 1)
-    print("Humidity: "+humidity+"%")
-    lcd.write("Humidity: "+humidity+"%")
-    lcd.set_cursor_position(0, 2)
-    print("Rain: "+rain+"%")
-    lcd.write("Rain: "+rain+"%")
+    PapirusText.write("Temp: "+temp+" C")
+#    lcd.set_cursor_position(0, 1)
+#    print("Humidity: "+humidity+"%")
+#    lcd.write("Humidity: "+humidity+"%")
+#    lcd.set_cursor_position(0, 2)
+#    print("Rain: "+rain+"%")
+#    lcd.write("Rain: "+rain+"%")
 except:
-    lcd.write("Connection Error")
+    PapirusText.write("Connection Error")
 while 1:
     updateWeather()
-    rainWarning()
-    graph()
     sleep(120)
