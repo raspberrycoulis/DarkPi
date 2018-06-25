c6b167d3834fb84cc5abd3e92352634237a20a53
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2018-06-25 13:58:39 +0100

    UV index and rain updates
    The UV index is now forecast and changes the backlight colour depending on the severity. Rain probability is now displayed on the graph LEDs instead.

--- darksky-dot3k.py
+++ darksky-dot3k.py
@@ -20,78 +20,79 @@ try:
     import forecastio
 except ImportError:
     exit("This script requires the forecastio module\nInstall with: sudo pip install forecastio")
-
-# Get the forecast details from Dark Sky
-forecast = forecastio.load_forecast(api_key,latitude,longitude)
-current = forecast.currently()
-
-# Get the temperature, humidity and chance of rain then
-# convert to strings to display on DOT3K.
-temp = current.temperature
-temp = str(temp)
-humidity = current.humidity*100
-humidity = str(humidity)
-rainInt = current.precipProbability*100
-rain = str(rainInt)
-
-def updateWeather():
-    forecast = forecastio.load_forecast(api_key,latitude,longitude)
-
+    
 def rainWarning():
-    if rainInt <= 33:
-        backlight.rgb(165, 255, 0)    # Green (good)
-    elif (rainInt > 34) and (rainInt <= 74):
-        backlight.rgb(226, 111, 11) # Orange (warning)
-    else:
-        backlight.rgb(0, 225, 255)  # Blue (you'll get wet!)
-
-def graph():
-    if rainInt ==0:
+    forecast = forecastio.load_forecast(api_key,latitude,longitude)
+    current = forecast.currently()
+    rain = current.precipProbability*100
+    if rain ==0:
         backlight.set_graph(0)
-    elif (rainInt >1) and (rainInt <=9):
+    elif (rain >1) and (rain <=9):
         backlight.set_graph(0.1)
-    elif (rainInt >= 10) and (rainInt <=19):
+    elif (rain >= 10) and (rain <=19):
         backlight.set_graph(0.2)
-    elif (rainInt >= 20) and (rainInt <=29):
+    elif (rain >= 20) and (rain <=29):
         backlight.set_graph(0.3)
-    elif (rainInt >= 30) and (rainInt <=39):
+    elif (rain >= 30) and (rain <=39):
         backlight.set_graph(0.4)
-    elif (rainInt >= 40) and (rainInt <=49):
+    elif (rain >= 40) and (rain <=49):
         backlight.set_graph(0.5)
-    elif (rainInt >= 50) and (rainInt <=59):
+    elif (rain >= 50) and (rain <=59):
         backlight.set_graph(0.6)
-    elif (rainInt >= 60) and (rainInt <=69):
+    elif (rain >= 60) and (rain <=69):
         backlight.set_graph(0.7)
-    elif (rainInt >= 70) and (rainInt <=79):
+    elif (rain >= 70) and (rain <=79):
         backlight.set_graph(0.8)
-    elif (rainInt >= 80) and (rainInt <=89):
+    elif (rain >= 80) and (rain <=89):
         backlight.set_graph(0.9)
     else:
         backlight.set_graph(1.0)
 
-# Press the button on the joystick to exit
-@nav.on(nav.BUTTON)
-def handle_button(pin):
-    lcd.clear()
-    backlight.rgb(0, 0, 0)
-    backlight.set_graph(0)
-    os._exit(1)
+def display():
+    forecast = forecastio.load_forecast(api_key,latitude,longitude)
+    current = forecast.currently()
+    temp = current.temperature
+    temp = str(temp)
+    humidity = current.humidity*100
+    humidity = str(humidity)
+    rain = current.precipProbability*100
+    rain = str(rain)
+    uvIndex = current.uvIndex
+    uv = str(uvIndex)
+    if uvIndex <=2.9:
+        backlight.rgb(90, 148, 35)  # Green (low)
+    elif (uvIndex >=3) and (uvIndex <=5.9):
+        backlight.rgb(241, 227, 54) # Yellow (moderate)
+    elif (uvIndex >=6) and (uvIndex <=7.9):
+        backlight.rgb(217, 90, 18)  # Orange (high)
+    elif (uvIndex >=8) and (uvIndex <=10.9):
+        backlight.rgb(185, 2, 34)   # Red (very high)
+    else:
+        backlight.rgb(99, 74, 195)  # Violet (extreme)
+    try:
+        lcd.clear()
+        lcd.set_cursor_position(0, 0)
+        print("Temperture: "+temp+" C")
+        lcd.write("Temp: "+temp+"\xf2C")
+        lcd.set_cursor_position(0, 1)
+        print("Humidity: "+humidity+"%")
+        lcd.write("Humidity: "+humidity+"%")
+        lcd.set_cursor_position(0, 2)
+        print("Rain: "+rain+"%")
+        lcd.write("Rain: "+rain+"%")
+        print("UV Index: "+uv+"")
+    except:
+        lcd.write("Connection Error")
+    
+    # Press the button on the joystick to exit
+    @nav.on(nav.BUTTON)
+    def handle_button(pin):
+        lcd.clear()
+        backlight.rgb(0, 0, 0)
+        backlight.set_graph(0)
+        os._exit(1)
 
-try:
-    lcd.clear()
-    lcd.set_cursor_position(0, 0)
-    print("Temperture: "+temp+" °C")
-    lcd.write("Temp: "+temp+" C")
-    lcd.set_cursor_position(0, 1)
-    print("Humidity: "+humidity+"%")
-    lcd.write("Humidity: "+humidity+"%")
-    lcd.set_cursor_position(0, 2)
-    print("Rain: "+rain+"%")
-    lcd.write("Rain: "+rain+"%")
-except:
-    lcd.write("Connection Error")
-while 1:
-    updateWeather()
+while True:
     rainWarning()
-    graph()
-    sleep(120)
+    display()
+    sleep(300)    # 5 minutes
